c1aa1f4ce6387c9ae11d804c1b99982fbde61eb2
LeeGwangSeok <zxc8661@naver.com>
2025-09-05 15:04:44 +0900

    텍스트 입력 작성중

--- src/components/TodoCreateButton.tsx
+++ src/components/TodoCreateButton.tsx
@@ -2,14 +2,30 @@
 import "./styled-components.css";
 import  {useState}  from "react";
 import { MdAdd, MdClose } from "react-icons/md";
-
+import {useTodoDispatch} from "../TodoContext";
 
 export default function TodoCreateButton(){
-
+    const dispatch = useTodoDispatch();
     const [open,setOpen] = useState(false);
+    const [text,setText] = useState("");
 
     const onClick = () => {
         setOpen(!open);
+    }
+
+    const onChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setText(e.target.value)
+    }
+
+    const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const value = text.trim();
+        if(!value){
+            return;
+        }
+        dispatch({type:'CREATE',text:value})
+        setText("");
+
     }
 
 
@@ -17,9 +33,9 @@ export default function TodoCreateButton(){
         <>
         {open &&(
             <div className="insertFormPositioner">
-                <div className="insertForm">
-                    <input className="insertFormInput" placeholder="할일 입력후 enter를 누르세요" />
-                </div>
+                <form className="insertForm" onSubmit={onSubmit}>
+                    <input className="insertFormInput" placeholder="할일 입력후 enter를 누르세요" value = {text} onChange={onChange} />
+                </form>
             </div>
         )}
         <button className={`todo-createButton ${open ? "open" : ""}`} onClick={onClick}>
